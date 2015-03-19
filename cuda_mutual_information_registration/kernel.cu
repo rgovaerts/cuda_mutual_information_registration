8353395897b61dc7e58f6163f77e979345881caa
rgovaerts <rem.govaerts@hotmail.com>
2015-03-19 15:13:27 +0100

    just testing git with VS2013

--- cuda_mutual_information_registration/kernel.cu
+++ cuda_mutual_information_registration/kernel.cu
@@ -14,7 +14,7 @@ __global__ void addKernel(int *c, const int *a, const int *b)
 
 int main()
 {
-	//COOL
+	//hey
     const int arraySize = 5;
     const int a[arraySize] = { 1, 2, 3, 4, 5 };
     const int b[arraySize] = { 10, 20, 30, 40, 50 };
