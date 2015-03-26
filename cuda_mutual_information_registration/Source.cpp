8a27bd92b465c6f7ed701b0977eaffc9e4cbe36b
Sagabrielli <Sagabrielli@gmail.com>
2015-03-26 11:59:08 +0100

    Première ébauche traitement image

--- cuda_mutual_information_registration/Source.cpp
+++ cuda_mutual_information_registration/Source.cpp
@@ -1,10 +1,10 @@
 
-//#include "opencv2\core\core.hpp"
-//#include "opencv2\highgui\highgui.hpp"
-//#include <iostream>
+#include "opencv2\core\core.hpp"
+#include "opencv2\highgui\highgui.hpp"
+#include <iostream>
+#include "opencv\cv.h"
 
-
-//using namespace cv;
+using namespace cv;
 
 
 
@@ -13,6 +13,38 @@
 
 /*int main()
 {
-	//Mat image = imread()
+	Mat image = imread("Lolol.png", CV_LOAD_IMAGE_COLOR);
+	Mat grayscale;
+	cvtColor(image, grayscale, CV_BGR2GRAY);
+
+
+	int hgt = grayscale.rowRange;
+	int wdt = grayscale.colRange;
+
+	int imagecut[200][200];
+
+	Mat image1(Size(wdt, hgt), CV_8UC3);
+	
+	int colCheck;
+	int rowCheck;
+	int templateCOL = 50;
+	int templateROW = 50;
+	colCheck = wdt%templateCOL;
+	rowCheck = hgt%templateROW;
+	
+	// A compléter encore ( rajouter 2 if ainsi que completer le premier pour qu'il "découpe" bien en carré et pas en ligne (même si on peut le stoquer sous forme de vecteur dans un tableau )
+	if (colCheck == 0 && rowCheck == 0) // sert a check si la taille choisie pour l'image découpée permet de recouvrir l'image complète de manière simple
+	{ 
+		for (int i = 0; i < wdt; i++)
+		{
+			for (int j = 0; j < hgt; j++)
+			{
+						grayscale.row(j).col(i).copyTo(image1.row(j).col(i));  
+			}
+		}
+	}
+
+
+
 	return 0;
 }*/
